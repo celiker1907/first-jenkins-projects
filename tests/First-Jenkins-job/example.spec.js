9131eed65b923918301fe6de43d1f2e73d18e7e1
celikerCydeo <celik+celik.eren57@hotmail.com>
2024-09-29 08:33:55 -0700

    first-jenkin-project update

--- tests/First-Jenkins-job/example.spec.js
+++ tests/First-Jenkins-job/example.spec.js
@@ -9,6 +9,8 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
+
+
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
